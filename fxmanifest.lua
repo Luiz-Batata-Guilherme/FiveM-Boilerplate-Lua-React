491479322c90c380f220a580c729d10368c18a72
Luiz Guilherme <37917447+Luiz-Batata-Guilherme@users.noreply.github.com>
2025-03-31 20:46:12 -0300

    initialize project with Vite, React, and TypeScript boilerplate

---
+++ fxmanifest.lua
@@ -0,0 +1,37 @@
+name 'FiveM Lua&ReactJs BoilerPlate QBox'
+author 'Lady Batata'
+version '0.1.0'
+license 'MIT'
+repository 'https://github.com/Luiz-Batata-Guilherme/Tigrinho'
+description 'BoilerPlate for FiveM framwork QBox, Lua and ReactJs, with ox_lib and qbx_core'
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+ui_page 'dist/web/index.html'
+
+files {
+
+}
+
+ox_lib 'locale'
+
+shared_files {
+	'shared/config.lua',
+	-- '@ox_lib/init.lua', -- Provides the ox_lib global table with various utility functions. | https://overextended.dev/ox_lib
+	-- '@qbx_core/modules/lib.lua', -- Provides the qbx global table with various utility functions. | https://docs.qbox.re/resources/qbx_core/modules/lib
+	-- '@qbx_core/modules/logger.lua', -- Adds a logger that logs to Discord if a webhook is given, and with ox_lib otherwise. | https://docs.qbox.re/resources/qbx_core/modules/logger
+	-- '@qbx_core/modules/hooks.lua', -- Event hooks allow 3rd party resources to define new behaviour without modifying your resource directly. | https://docs.qbox.re/resources/qbx_core/modules/hooks
+}
+
+client_scripts {
+	'client/client.lua',
+}
+
+server_scripts {
+	-- '@oxmysql/lib/MySQL.lua', --Discomment this if you want to use oxmysql | https://overextended.dev/oxmysql
+	'server/server.lua',
+}
+
+
+use_experimental_fxv2_oal 'yes'
